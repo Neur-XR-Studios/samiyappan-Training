4b2fd3dffb6e547e3302499711b2df96fb5c20a7
SAMIYAPPAN <samisamy4597m@gmail.com>
2023-08-22 15:17:24 +0530

    Color Change

--- Assets/Scripts/Loadscene.cs
+++ Assets/Scripts/Loadscene.cs
@@ -6,7 +6,20 @@ using UnityEngine.SceneManagement;
 public class Loadscene : MonoBehaviour
 {
 
-   
+
+    public Material materialToChange;   
+    public Color oldColor = Color.green;
+    public Color oldColor2 = Color.blue;
+    public Color oldColor3 = Color.yellow;
+
+    public static Loadscene instance;
+    public void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
 
     public void Level()
     {
@@ -38,4 +51,27 @@ public class Loadscene : MonoBehaviour
         Application.Quit();
         CoinCollections.instance.ResetScoreToPrevious();
     }
+   
+    public void Changegreen()
+    {
+        if (materialToChange != null)
+        {
+            materialToChange.color = oldColor;
+        }
+    }
+    public void ChangeBlue()
+    {
+        if (materialToChange != null)
+        {
+            materialToChange.color = oldColor2;
+        }
+    }
+    public void ChangeYellow()
+    {
+        if (materialToChange != null)
+        {
+            materialToChange.color = oldColor3;
+        }
+    }
+
 }
