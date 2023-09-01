e46748c867085cb3640b0a703fe648fa109c95e7
SAMIYAPPAN <samisamy4597m@gmail.com>
2023-09-01 12:43:53 +0530

    Revert "Update CoinCollections.cs"
    This reverts commit 65ae333f320ade6432cc9035a7304a554f0533a0.

--- Assets/Scripts/CoinCollections.cs
+++ Assets/Scripts/CoinCollections.cs
@@ -65,86 +65,21 @@ public class CoinCollections : MonoBehaviour
       /*  Score = Score + 20;
         Scorename.text = Score.ToString();*/
     }
-
+   
     public void Fuel()
-     {
-       CurrentFuel -= UseFuel * Time.deltaTime;
+    {
+      CurrentFuel -= UseFuel * Time.deltaTime;
 
-         FuelSlider.value = CurrentFuel;
-       if (CurrentFuel<MinValue)
-       {
-             CurrentFuel = 0;
-       }
-     }
+        FuelSlider.value = CurrentFuel;
+      if (CurrentFuel<MinValue)
+      {
+            CurrentFuel = 0;
+      }
+    }
 
-     public void Refill()
-     {
-         CurrentFuel = maxFuel;
-     }
-
-
-
-    /* 
-     public static CoinCollections instance;
-     public TMP_Text Scorename;
-     public int Score = 0;
-     public Slider FuelSlider;
-     public float CurrentFuel = 10f;
-     public float maxFuel = 10f;
-     public float UseFuel = 1f;
-     public float MinValue = 1f;
-     public string Names;
-     private int previousScore;
-
-     private void Start()
-     {
-         if (instance == null)
-         {
-             instance = this;
-         }
-         previousScore = PlayerPrefs.GetInt("PreviousScore", 0);
-
-         // Update the current score display
-         Score = previousScore;
-         Scorename.text = Score.ToString();
-
-     }
-     public void ResetScoreToPrevious()
-     {
-         PlayerPrefs.SetInt("PreviousScore", Score);
-     }
-
-
-     public void Scoreing()
-     {
-         Score = Score + 1;
-         Scorename.text = Score.ToString();
-     }
-
-     public void PickupMoney()
-     {
-         Score = Score + 10;
-         Scorename.text = Score.ToString();
-     }
-     public void DropMoney()
-     {
-         Score = Score + 20;
-         Scorename.text = Score.ToString();
-     }
-     public void IngnoreMoney()
-     {
-         Score = Score - 10;
-         Scorename.text = Score.ToString();
-     }
-     public void Fuel()
-     {
-         CurrentFuel -= UseFuel * Time.deltaTime;
-
-         FuelSlider.value = CurrentFuel;
-         if (CurrentFuel < MinValue)
-         {
-             CurrentFuel = 0;
-         }
-     }*/
-
+    public void Refill()
+    {
+        CurrentFuel = maxFuel;
+    }
+   
 }
